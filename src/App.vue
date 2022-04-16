302836d266c2c27aec4d46789ab45a0d615a5ecd
veypi <veypi@qq.com>
2022-04-16 11:12:18 +0800

    init

---
+++ src/App.vue
@@ -0,0 +1,23 @@
+<style>
+#app {
+  @apply h-full w-full flex justify-center items-center;
+}
+</style>
+
+<template>
+  <router-view v-slot="{ Component }">
+    <transition mode="out-in" enter-active-class="animate__fadeIn"
+                leave-active-class="animate__fadeOut">
+      <component class="animate__animated animate__faster" :is="Component"
+      ></component>
+    </transition>
+  </router-view>
+</template>
+
+<script setup lang="ts">
+import {useUserStore} from '@/store/user'
+
+let user = useUserStore()
+user.setUser()
+</script>
+
